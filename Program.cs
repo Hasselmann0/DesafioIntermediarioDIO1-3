39aaef41636fe64a8ca5bb3606afa4ff75ee7eb6
Isaac Bastos <isaacbilac920@gmail.com>
2023-01-27 11:24:42 -0300

    Stored

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        int tamanhoArray = int.Parse(Console.ReadLine());
+        
+        int[] num = new int[tamanhoArray];
+    
+// TODO: Crie as outras condições necessárias para a resolução do desafio:
+        for (int i = 0; i < tamanhoArray; i++)
+        {
+            num[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine(MajorityElement(num));
+        
+    }
+    public static int MajorityElement(int[] nums)
+    {
+        int major = nums[0];
+        int count = 0;
+        for (int i = 0; i != nums.Length; i++)
+        {
+            if (count == 0)
+            {
+                major = nums[i];
+                count++;
+            }
+            else
+            {
+                if (major == nums[i])
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+        }
+        return major;
+    }
+}
+
